3b0163db4abf984d256f0fc0442d88c549339936
Hung Q. Nguyen <hnguyenworkstation@gmail.com>
2016-01-08 12:34:33 -0500

    Initial Files

---
+++ BookStore-Management/BookStore-Management/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookStore_Management.MvcApplication" Language="C#" %>
---
+++ BookStore-Management/BookStore-Management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookStore_Management.Startup))]
+namespace BookStore_Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
