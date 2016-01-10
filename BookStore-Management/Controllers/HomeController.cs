79644851da077a80e92af085ed3d264e7e07d95b
Hung Q. Nguyen <hnguyenworkstation@gmail.com>
2016-01-10 18:53:40 -0500

    Book Cover Images

--- BookStore-Management/Controllers/HomeController.cs
+++ BookStore-Management/Controllers/HomeController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookStore_Management.Models;
 
 namespace BookStore_Management.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+
+        // Create a Database
+        QuanLyBanSachEntities db = new QuanLyBanSachEntities();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public PartialViewResult NewBooksPartial()
+        {
+            var listNewBooks = db.Saches.Take(3).ToList();
+            return PartialView(listNewBooks);
         }
     }
 }
