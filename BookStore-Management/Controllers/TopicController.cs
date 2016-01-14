e07c8a0e324e6817f8699a5600e424ee9c756dc8
Hung Q. Nguyen <hnguyenworkstation@gmail.com>
2016-01-13 21:56:37 -0500

    Get a ViewResult for books which have same Topic

--- BookStore-Management/Controllers/TopicController.cs
+++ BookStore-Management/Controllers/TopicController.cs
@@ -17,5 +17,27 @@ namespace BookStore_Management.Controllers
         {
             return PartialView(db.ChuDes.Take(5).ToList());
         }
+
+        /*Find books by Topic*/
+        public ViewResult booksByTopic(int topicID)
+        {
+            // Check if the topic ID is valid
+            ChuDe topic = db.ChuDes.SingleOrDefault(n => n.MaChuDe == topicID);
+            if(topic == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+
+            // Get list of books by topic
+            List<Sach> lstBook = db.Saches.Where(n => n.MaChuDe == topicID).OrderBy(n=>n.GiaBan).ToList();
+
+            if(lstBook.Count == 0)
+            {
+                ViewBag.Sach = "Couldn't Find any book matches this topic";
+            }
+            return View(lstBook);
+
+        }
     }
 }
